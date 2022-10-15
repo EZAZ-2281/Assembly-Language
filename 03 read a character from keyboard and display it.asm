1eecc1b3280b0a28042c7e1569adf121f625d947
Golam Kibria Ezaz <golamkibria3435@gmail.com>
2022-10-15 12:25:47 +0600

    asm

--- 03 read a character from keyboard and display it.asm
+++ 03 read a character from keyboard and display it.asm
@@ -14,9 +14,9 @@ main proc
     
     ;go to a new line
     mov ah,2        
-    mov dl,0Dh      ;carriage return 
+    mov dl,10       ;carriage return 
     int 21h         ;execute carriage return
-    mov dl,0Ah      ;line feed
+    mov dl,13       ;line feed
     int 21h         ;execute line feed
     
     ;display character
@@ -26,8 +26,8 @@ main proc
     ;return to dos
     mov ah,4ch      ;dos exit function
     int 21h
-main endp
-    end main 
+    main endp
+end main 
     
 ;The rason we had to move the input character from al to bl is that the int 21h, function 2, changes al. 
 ;When a program termintes, it should return control to DOS. This can be accomplished by executing int 21h, function 4ch.    
